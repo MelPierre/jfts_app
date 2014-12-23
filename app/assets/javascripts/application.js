f32bc0b13155bd3a53d0b3f114a545607b68916a
mel pierre <mel.pierre@digital.justice.gov.uk>
2014-12-23 15:31:20 +0000

    new fees project

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,28 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+
+// Project assets
+//= require visitors
+
+// Candidates for re-usable components
+//= require modules/moj.autocomplete
+//= require modules/moj.hijacks
+//= require modules/moj.timeout-prompt
+//= require modules/moj.submit-once
+
+// Assets without a better place
+//= require shame
+
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require_tree .
